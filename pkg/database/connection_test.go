980161dbb6760f1ee10a378b1e6197a089c7e769
Muhammad Zuhrul Umam <zuhrulu@gmail.com>
2019-05-02 09:55:49 +0700

    [umam] add test queryok

--- pkg/database/connection_test.go
+++ pkg/database/connection_test.go
@@ -9,6 +9,6 @@ import (
 func TestConnection_PingOK(t *testing.T) {
 	dbURL := os.Getenv("LEARNING_GO_DB_URL")
 	err := ConfigureTest(dbURL)
-	
+	defer Quit()
 	assert.Nil(t, err, "they should err nil when try to connect")
 }
--- pkg/database/query_test.go
+++ pkg/database/query_test.go
@@ -1,1 +1,15 @@
 package database
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestQuery_QueryOK(t *testing.T) {
+	dbURL := os.Getenv("LEARNING_GO_DB_URL")
+	err := ConfigureTest(dbURL)
+
+	_, err = Query(`select 1`)
+	assert.Nil(t, err, "it should nil on error")
+}

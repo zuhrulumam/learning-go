9eb0b496ad15c651a56439abfd329271a8633f7b
Muhammad Zuhrul Umam <zuhrulu@gmail.com>
2019-05-03 14:22:10 +0700

    [umam] delete user

---
+++ pkg/users/delete_user.go
@@ -0,0 +1,28 @@
+package users
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/zuhrulumam/learning-go/pkg/database"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+)
+
+func v1DeleteUsersHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("deleting user")
+
+	id := chi.URLParam(r, "id")
+
+	query := `
+		delete from users where id = ?
+	`
+	_, err := database.Exec(query, id)
+	if err != nil {
+		log.Println("error creating " + err.Error())
+	}
+
+	render.Status(r, 200)
+	render.JSON(w, r, "success deleting user")
+}
--- pkg/users/routes.go
+++ pkg/users/routes.go
@@ -10,44 +10,8 @@ import (
 
 // Routes list user routes
 func Routes(r chi.Router) {
-
-	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-
-	// 	rows, err := database.Query(`select * from test`)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-
-	// 	log.Println(rows)
-
-	// 	var (
-	// 		id   uint
-	// 		name string
-	// 	)
-
-	// 	type user struct {
-	// 		id   uint
-	// 		name string
-	// 	}
-
-	// 	var users []user
-	// 	for rows.Next() {
-	// 		err := rows.Scan(&id, &name)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 		log.Println(id, name)
-	// 		currUser := user{
-	// 			id:   id,
-	// 			name: name,
-	// 		}
-	// 		users = append(users, currUser)
-	// 	}
-
-	// 	log.Println(users)
-	// 	log.Println("Get / from users")
-	// })
 	r.Get("/", v1ListUsersHandler)
 	r.Post("/", v1CreateUserHandler)
 	r.Put("/{id}", v1UpdateUsersHandler)
+	r.Delete("/{id}", v1DeleteUsersHandler)
 }
